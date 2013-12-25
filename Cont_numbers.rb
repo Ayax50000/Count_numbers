775204d2d9e5bc8e745e0a5e452f718bba2c9adb
Ayax50000 <fjlsoto@gmail.com>
2013-12-25 17:16:11 -0600

    comments removed

--- Cont_numbers.rb
+++ Cont_numbers.rb
@@ -1,17 +1,14 @@
 class Numeros
   def self.check
-      # se crea un hash vacio
       hash =  {}
       valores = [1,1,1,1,2,3,4,5,66,7,8,9,8,7,6,6,5,5,4,4,3,3,2,1,2,3,4,5,6,7,8,9
                ,8,7,6,5,4,3,2,1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,1,2,3,3,3,3,3,3,3,4
                ,4,4,5,5,5,5,6,6,5,5,5,66,66854789435,4,4,4,4,3,3,2,3,4,5,6,5,4,
                 2,222,3,3,4,6,7,8,9,7,5,4,3,3]
-      # iteracion para evaluar los numeros
       valores.each do |valor_actual|
         hash[valor_actual] = 0 unless hash.keys.include? valor_actual
         hash[valor_actual] += 1 if hash.keys.include? valor_actual
       end
-      # iteracion para mostrar el resultado
       hash.each do |numero_actual|
           puts "#{numero_actual[0]} = #{numero_actual[1]}"
       end
