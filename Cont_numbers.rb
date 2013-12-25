b21b3304749618212861304b834f2bcfc2651931
Ayax50000 <fjlsoto@gmail.com>
2013-12-25 17:36:29 -0600

    error in assignation of values for variable valores

--- Cont_numbers.rb
+++ Cont_numbers.rb
@@ -1,10 +1,10 @@
 class Numeros
   def self.check
       hash =  {}
-      valores = [1,1,1,1,2,3,4,5,66,7,8,9,8,7,6,6,5,5,4,4,3,3,2,1,2,3,4,5,6,7,8,9
-               ,8,7,6,5,4,3,2,1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,1,2,3,3,3,3,3,3,3,4
-               ,4,4,5,5,5,5,6,6,5,5,5,66,66854789435,4,4,4,4,3,3,2,3,4,5,6,5,4,
-                2,222,3,3,4,6,7,8,9,7,5,4,3,3]
+      valores =  [1,1,1,1,2,3,4,5,66,7,8,9,8,7,6,6,5,5,4,4,3,3,2,1,2,3,4,5,6,7,8,9]
+      valores.push(8,7,6,5,4,3,2,1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,1,2,3,3,3,3,3,3,3,4)
+      valores.push(4,4,5,5,5,5,6,6,5,5,5,66,66854789435,4,4,4,4,3,3,2,3,4,5,6,5,4)
+      valores.push(2,222,3,3,4,6,7,8,9,7,5,4,3,3)
       valores.each do |valor_actual|
         hash[valor_actual] = 0 unless hash.keys.include? valor_actual
         hash[valor_actual] += 1 if hash.keys.include? valor_actual
@@ -12,6 +12,6 @@ class Numeros
       hash.each do |numero_actual|
           puts "#{numero_actual[0]} = #{numero_actual[1]}"
       end
-      nil
+nil
   end
 end
