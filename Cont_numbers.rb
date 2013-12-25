4342ddfc6608e291ee89d5f7eaec9a553c9f08fe
Ayax50000 <fjlsoto@gmail.com>
2013-12-25 17:06:54 -0600

    name of variable called array changed to valores

--- Cont_numbers.rb
+++ Cont_numbers.rb
@@ -4,12 +4,12 @@ class Numeros
   def self.check
       # se crea un hash vacio
       hash =  {}
-      array = [1,1,1,1,2,3,4,5,66,7,8,9,8,7,6,6,5,5,4,4,3,3,2,1,2,3,4,5,6,7,8,9
+      valores = [1,1,1,1,2,3,4,5,66,7,8,9,8,7,6,6,5,5,4,4,3,3,2,1,2,3,4,5,6,7,8,9
                ,8,7,6,5,4,3,2,1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,1,2,3,3,3,3,3,3,3,4
                ,4,4,5,5,5,5,6,6,5,5,5,66,66854789435,4,4,4,4,3,3,2,3,4,5,6,5,4,
                 2,222,3,3,4,6,7,8,9,7,5,4,3,3]
       # iteracion para evaluar los numeros
-      array.each do |x|
+      valores.each do |x|
         hash[x] = 0 unless hash.keys.include?x
         hash[x] += 1 if hash.keys.include?x
       end
