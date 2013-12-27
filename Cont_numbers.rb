30a13810dbfea40a0e5b74d94508e88ee07235fe
Ayax50000 <fjlsoto@gmail.com>
2013-12-26 18:49:23 -0600

    Update Cont_numbers.rb

--- Cont_numbers.rb
+++ Cont_numbers.rb
@@ -5,6 +5,7 @@ class Contador
                   8,7,6,5,4,3,2,1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,1,2,3,3,3,3,3,3,3,4,
                   4,4,5,5,5,5,6,6,5,5,5,66,66854789435,4,4,4,4,3,3,2,3,4,5,6,5,4,2,
                   222,3,3,4,6,7,8,9,7,5,4,3,3]
+      valores.compact
       valores.each do |indice|
           resultados[indice] = (resultados.keys.include? indice)? (resultados[indice] +=1) : 0
       end
